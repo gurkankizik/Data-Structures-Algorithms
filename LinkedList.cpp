ce7e14e646e65b68e71f1566458580c7ea4a24b9
gurkankizik <105675855+gurkankizik@users.noreply.github.com>
2023-03-30 22:52:16 +0300

    Formatted and Beautified code

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,67 +1,64 @@
 #include <iostream>
+
 using namespace std;
 
 class Node {
-public:
-	int data;
-	Node* next;
+  public: int data;
+  Node * next;
 };
 
 class SinglyLinkedList {
-public:
-	SinglyLinkedList() {
-		head = NULL;
-	}
-	void insert(int value) {
-		Node* newNode = new Node();
-		newNode->data = value;
-		newNode->next = head;
-		head = newNode;
+  public: SinglyLinkedList() {
+    head = NULL;
+  }
+  void insert(int value) {
+    Node * newNode = new Node();
+    newNode -> data = value;
+    newNode -> next = head;
+    head = newNode;
 
-	}
-	void remove(int value) {
-		Node* current = head;
-		Node * previous = NULL;
+  }
+  void remove(int value) {
+    Node * current = head;
+    Node * previous = NULL;
 
-		while (current != NULL) {
-			if (current->data = value) {
-				if (previous == NULL) {
-					head = current->next;
-				}
-				else {
-					previous->next = current->next;
-				}
-				delete current;
-				return;
-			}
-			previous = current;
-			current = current->next;
-		}
-	}
-	void print() {
-		Node* current = head;
-		if (head == NULL) {
-			cout << "Empty List" << endl;
-			return;
-		}
-		while (current != NULL) {
-			cout << current->data << " ";
-			current = current->next;
-		}
-		cout << endl;
-	}
-private:
-	Node* head;
+    while (current != NULL) {
+      if (current -> data = value) {
+        if (previous == NULL) {
+          head = current -> next;
+        } else {
+          previous -> next = current -> next;
+        }
+        delete current;
+        return;
+      }
+      previous = current;
+      current = current -> next;
+    }
+  }
+  void print() {
+    Node * current = head;
+    if (head == NULL) {
+      cout << "Empty List" << endl;
+      return;
+    }
+    while (current != NULL) {
+      cout << current -> data << " ";
+      current = current -> next;
+    }
+    cout << endl;
+  }
+  private: Node * head;
 };
 
 int main() {
-	SinglyLinkedList myList;
+  SinglyLinkedList myList;
 
-	myList.insert(5);
-	myList.insert(10);
-	myList.insert(15);
-	myList.insert(20);
-	myList.print();
-	myList.remove(15);
-	myList.print();
+  myList.insert(5);
+  myList.insert(10);
+  myList.insert(15);
+  myList.insert(20);
+  myList.print();
+  myList.remove(15);
+  myList.print();
 }
